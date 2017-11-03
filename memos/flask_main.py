0ca835e0ba4cf1ab222ae5a1b2e835534de54de7
John <recuxer@blackmamba.localdomain>
2017-11-02 17:02:58 -0700

    added minor features

--- memos/flask_main.py
+++ memos/flask_main.py
@@ -17,7 +17,7 @@ from flask import g
 from flask import render_template
 from flask import request
 from flask import url_for
-#import json
+from flask import flash
 import logging
 import sys
 
@@ -28,29 +28,24 @@ from dateutil import tz  # For interpreting local times
 # Mongo database
 from pymongo import MongoClient
 
+# Config Args
 import config
 CONFIG = config.configuration()
 
+#mongo URL
 MONGO_CLIENT_URL = "mongodb://{}:{}@{}:{}/{}".format(
     CONFIG.DB_USER,
     CONFIG.DB_USER_PW,
     CONFIG.DB_HOST, 
     CONFIG.DB_PORT, 
     CONFIG.DB)
-
 print("Using URL '{}'".format(MONGO_CLIENT_URL))
 
-###
 # Globals
-###
-
 app = flask.Flask(__name__)
 app.secret_key = CONFIG.SECRET_KEY
 
-####
 # Database connection per server process
-###
-
 try: 
     dbclient = MongoClient(MONGO_CLIENT_URL)
     db = getattr(dbclient, CONFIG.DB)
@@ -60,7 +55,7 @@ except:
     print("Failure opening database.  Is Mongo running? Correct password?")
     sys.exit(1)
 
-###
+###########
 # Pages
 ###
 
@@ -71,35 +66,7 @@ def index():
     g.memos = get_memos()
     for memo in g.memos: 
         app.logger.debug("Memo: " + str(memo))
-  
     return flask.render_template('index.html')
-
-
-# We don't have an interface for creating memos yet
-@app.route("/create", methods=["POST"])
-def create():
-    app.logger.debug("Create")
-    memo = flask.request.form["memo"]
-    datetime = flask.request.form["date"] + ' ' + flask.request.form["time"]
-    memotime = arrow.get(datetime, 'YYYY-MM-DD HH:mm')
-    newmemo = { "type": "dated_memo",
-                "date": memotime.naive,
-                "text": memo
-              }
-    print('added memo with date: ' + str(newmemo["date"]))
-    collection.insert(newmemo)
-     
-    return flask.redirect(flask.url_for("index"))
-
-#@app.route("/submit_memo", methods=["POST"])
-#def submit_memo(): 
-@app.route("/remove", methods=["POST"])
-def remove():
-    app.logger.debug("Remove")
-    memodate = arrow.get(flask.request.form["removememo"]).naive
-    collection.delete_one({ "date": memodate })
-    print('removing memo with date: ' + str(memodate))
-    return flask.redirect(flask.url_for("index")) 
 
 @app.errorhandler(404)
 def page_not_found(error):
@@ -108,12 +75,40 @@ def page_not_found(error):
                                  badurl=request.base_url,
                                  linkback=url_for("index")), 404
 
+##########
+# form handlers
+###
+
+@app.route("/create", methods=["POST"])
+def create():
+    app.logger.debug("Create")
+    memo = flask.request.form["memo"]
+    cur_datetime = flask.request.form["date"] + ' ' + flask.request.form["time"]
+    #if user didn't enter required fields
+    if not memo or cur_datetime == ' ':
+        error = "memo not created. date, time, and memo field can't be empty!"
+        flash(error)
+    else:
+        memotime = arrow.get(cur_datetime, 'YYYY-MM-DD HH:mm')
+        newmemo = { "type": "dated_memo",
+                    "date": memotime.naive,
+                    "text": memo
+                  }
+        collection.insert(newmemo)
+    return flask.redirect(flask.url_for("index"))
+
+@app.route("/remove", methods=["POST"])
+def remove():
+    app.logger.debug("Remove")
+    memodate = arrow.get(flask.request.form["removememo"]).naive
+    collection.delete_one({ "date": memodate })
+    return flask.redirect(flask.url_for("index")) 
+
 #################
 #
 # Functions used within the templates
 #
 #################
-
 
 @app.template_filter( 'humanize' )
 def humanize_arrow_date( date ):
@@ -136,7 +131,6 @@ def humanize_arrow_date( date ):
         human = date
     return human
 
-
 #############
 #
 # Functions available to the page code above
@@ -148,18 +142,13 @@ def get_memos():
     can be inserted directly in the 'session' object.
     """
     records = [ ]
-    for record in collection.find( { "type": "dated_memo" } ):
+    for record in collection.find( { "type": "dated_memo" } ).sort('date'):
         record['date'] = arrow.get(record['date']).isoformat()
         del record['_id']
         records.append(record)
-        print('memo dates: ' + record['date'])
-    #print(records)
     return records 
-
 
 if __name__ == "__main__":
     app.debug=CONFIG.DEBUG
     app.logger.setLevel(logging.DEBUG)
     app.run(port=CONFIG.PORT,host="0.0.0.0")
-
-    
